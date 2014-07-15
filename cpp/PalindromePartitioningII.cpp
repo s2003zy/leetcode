83ab571d82c9057e2c1c13fb866d3caa3b783428
宋智洋 <s2003zy@qq.com>
2014-07-15 10:50:00 +0800

    Palindrome Partitioning II

---
+++ cpp/PalindromePartitioningII.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minCut(string s) {
+        if(s=="")return 0;
+        int sLen = s.size();
+        vector<int> dp(sLen,INT_MAX);
+        dp[0]=0;
+        vector<vector<int>> mark(sLen,vector<int>(sLen,false));
+        for (int i = 1 ; i < sLen ; i++ ) {
+            for (int j = i; j >=0; j--) {
+                
+                if(s[i]==s[j]&&( (i-j<2)||(mark[j+1][i-1]) ) )
+                {
+                    mark[j][i] = true;
+                    if(j>0)
+                        dp[i] = min(dp[i],dp[j-1]+1);
+                    else
+                        dp[i] = 0;
+                }
+            }
+        }
+        return dp[sLen-1];
+        
+    }
+};
